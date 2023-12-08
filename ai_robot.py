3d786dde711140cd5dae32bbccf91d660fb49c5b
NEivier <146806585+NEivier@users.noreply.github.com>
2023-12-08 20:11:28 +0800

    Add files via upload

---
+++ ai_robot.py
@@ -0,0 +1,80 @@
+import requests
+import json
+import asr
+import tts
+import sys
+from auto_record import auto_record
+
+
+class ai_robot:
+    API_KEY = 'kNpGoAVgiXIMH1z298Gvv1Q2'
+    SECRET_KEY = 'bWzYBEqE7FxGaYFkzmSaYaChpWRew29B'
+
+    # 生成一次message
+    def __init__(self):
+        self.messages = [
+            {"role": 'user', "content": "你好"},
+            {"role": "assistant", "content": "你好,有什么我可以帮助你的吗？"}
+        ]
+
+    def get_access_token(self):
+        url = 'https://aip.baidubce.com/oauth/2.0/token'
+        params = {'grant_type': 'client_credentials', 'client_id': self.API_KEY, 'client_secret': self.SECRET_KEY}
+        return str(requests.post(url, params=params).json().get('access_token'))
+
+    # 启动文心一言
+    def get_url(self):
+        url = 'https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/completions?access_token=' + self.get_access_token()
+        return url
+
+    ##将messages上传给文心一言，并获得答案
+    def get_assistant_reply(self, url):
+        payload = json.dumps({"messages": self.messages})
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        response = requests.post(url, headers=headers, data=payload)
+        # translate the answers by the robot
+        response_data = json.loads(response.text)
+        return response_data.get('result')
+
+    ##用户message扩充
+    def message_usr_append(self, question):
+        self.messages.append({"role": "user", "content": question})
+
+    ##机器人message扩充
+    def message_assistant_append(self, reply):
+        self.messages.append({'role': "assistant", 'content': reply})
+
+    ##开始录音，并进行智能回答
+    def start_chat(self):
+        url = self.get_url()
+        # start recording and convert the speech into text
+        Epoch = True
+        while Epoch:
+            micro = auto_record()
+            flag = micro.listen()  ##开始听
+            if flag:  ##如果声音强度足够
+                micro.record()
+                sound_file = micro.sound_flie_path()
+                question = asr.asr(sound_file)
+                # put the input by users into the message
+                self.message_usr_append(question)
+                assistant_reply = self.get_assistant_reply(url)
+                self.message_assistant_append(assistant_reply)
+                # convet text to speech
+                tts.tts(assistant_reply)
+                if str(question) == str("谢谢。"):
+                    Epoch = False
+            else:
+                Epoch = False
+
+
+if __name__ == '__main__':
+    AI = ai_robot()
+    AI.start_chat()
+
+
+
+
+
